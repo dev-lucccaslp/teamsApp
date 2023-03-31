4ec761af4c59315082bc9749066a8b64d85a6b6c
Lucas <luccaslp99@gmail.com>
2023-03-31 13:57:36 -0300

    Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { ThemeProvider } from 'styled-components';
-
-import { Groups } from '@screens/Groups';
-import theme from '@theme/index';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { ActivityIndicator } from 'react-native';
+
+import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
+import theme from '@theme/index';
 
 export default function App() {
  const [ fontsLoaded ] = useFonts({
@@ -14,7 +15,7 @@ export default function App() {
   return (
     <>
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator/>}
+      { fontsLoaded ? <Groups /> : <Loading/>}
     </ThemeProvider>
     </>
   );
