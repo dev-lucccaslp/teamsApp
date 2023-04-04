6b135d03508340b8a2c28981bd43649edf22a57c
Lucas <luccaslp99@gmail.com>
2023-04-04 15:22:31 -0300

    create new componente ButtonIcon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Container } from './styles';
 
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
+import { ButtonIcon } from '@components/ButtonIcon';
 
 export function Players() {
   return (
@@ -12,6 +13,8 @@ export function Players() {
         title='Nome da turma'
         subtitle='adicione a galera e separe os times'
       />
+
+      <ButtonIcon />
     </Container>
   );
 }
