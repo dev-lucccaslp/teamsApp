2822f3b3c7d4a04178cdd142641f4d829acab166
Lucas <luccaslp99@gmail.com>
2023-04-04 17:19:12 -0300

    create new component filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Form } from './styles';
 
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
 
 export function Players() {
   return (
@@ -14,7 +16,22 @@ export function Players() {
         subtitle='adicione a galera e separe os times'
       />
 
-      <ButtonIcon />
+      <Form>
+        <Input 
+          placeholder='Nome da pessoa'
+          autoCorrect={false}
+        />
+
+        <ButtonIcon 
+          icon='add'
+        />
+      </Form>
+
+      <Filter
+        title='Time A'
+        
+      />
+      
     </Container>
   );
 }
