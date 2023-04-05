ef9f9eacdab1b86c48877e357d0b8c705a48ab0f
Lucas <luccaslp99@gmail.com>
2023-04-05 09:07:52 -0300

    finished a component filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -9,11 +9,13 @@ import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
 import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Players() {
 
   const [ team, setTeam ] = useState('Time A');
-  const [ players, setPlayers] = useState(['Lucas', 'Juciano'])
+  const [ players, setPlayers] = useState([])
 
   return (
     <Container>
@@ -60,7 +62,21 @@ export function Players() {
             onRemove={() => {}}
           />
         )}
-      
+        ListEmptyComponent={ () => (
+          <ListEmpty
+            message='Não há pessoas nesse time'
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={[
+          {paddingBottom:100},
+          players.length === 0 && { flex: 1}
+        ]}
+      />
+
+      <Button
+        title='Remover Turma'
+        type='SECONDARY'
       />
 
     </Container>
