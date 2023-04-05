6c5c7f77ee4ea8edee66d5fafbfb310b06af7f82
Lucas <luccaslp99@gmail.com>
2023-04-05 08:58:14 -0300

    create a new component 'Player Card'

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Container, Form } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Container, Form, HeaderList, NumebersOfPlayers } from './styles';
 
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
 
 export function Players() {
+
+  const [ team, setTeam ] = useState('Time A');
+  const [ players, setPlayers] = useState(['Lucas', 'Juciano'])
+
   return (
     <Container>
       <Header showBackButton />
@@ -26,12 +33,36 @@ export function Players() {
           icon='add'
         />
       </Form>
-
-      <Filter
-        title='Time A'
-        
-      />
+      <HeaderList>
+        <FlatList 
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumebersOfPlayers>
+          {players.length}
+        </NumebersOfPlayers>
+      </HeaderList>
       
+      <FlatList 
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <PlayerCard 
+            name ={item}
+            onRemove={() => {}}
+          />
+        )}
+      
+      />
+
     </Container>
   );
 }
