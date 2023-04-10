dcf0e424ab033c92d727507faf65e8d9cfe16d64
Lucas <luccaslp99@gmail.com>
2023-04-10 09:09:41 -0300

    handling exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,8 @@
+import { Alert } from 'react-native';
 import {useState} from 'react';
 import { useNavigation } from '@react-navigation/native';
 
+import { AppError } from '@utils/AppError';
 import { groupCreate } from '../../storage/group/groupCreate';
 
 import { Header } from '@components/Header';
@@ -15,15 +17,22 @@ export function NewGroup() {
 
   const [ group, setGroup ] = useState('');
 
-   async function handleNew(){
+   async function handleNew() {
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma');
+      }
 
-    try{
-    
       await groupCreate(group)
       navigation.navigate('players', { group });
     
     } catch(error) {
-      console.log(error);
+      if(error instanceof AppError){
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
     } 
 
   }
